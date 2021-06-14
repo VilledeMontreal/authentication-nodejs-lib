c6164d6ffc8aa942a02d5a5db2474b7100c796f6
Morgan Martinet <morgan.martinet@montreal.ca>
2021-06-14 00:31:16 -0400

    chore: fix sonarqube warning and upgrade deps

--- packages/oidc-plugin-request/src/createSession.ts
+++ packages/oidc-plugin-request/src/createSession.ts
@@ -3,7 +3,6 @@
  * Licensed under the MIT license.
  * See LICENSE file in the project root for full license information.
  */
-// import { ILogger, IHttpDefaults } from '@villedemontreal/auth-core';
 import {
   createSession as coreCreateSession,
   IOidcClientConfig,
